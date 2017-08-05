f372c5527cb9e901c1527f51ea866a91ef7c8f20
Never、C <caolin@neverc.cn>
2017-08-05 17:18:30 +0800

    init sc

---
+++ Chat.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new DnsEndPoint("ja.youlijinfu.com",8080));
+            // client.Connect(new DnsEndPoint("localhost",8080));
+            // client.Connect(new IPEndPoint());
+            var buffer = new byte[1024];
+            var data = client.Receive(buffer);
+
+            // iptables -A INPUT -p tcp --dport  8080-j ACCEPT
+            System.Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+    }
+}
---
+++ Chat.Server/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+            server.Listen(10);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var client = server.Accept();
+                    System.Console.WriteLine("Client 请求：" + DateTime.Now);
+                    client.Send(Encoding.UTF8.GetBytes("Hello Server"));
+                }
+            });
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
