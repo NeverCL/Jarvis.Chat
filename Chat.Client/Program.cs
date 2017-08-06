be4ded04b7d947eb0e331f03a8a236768fb8b2a0
Never、C <caolin@never.cn>
2017-08-06 12:58:45 +0800

    socket wapper

--- Chat.Client/Program.cs
+++ Chat.Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Chat.Client
 {
@@ -14,9 +15,58 @@ namespace Chat.Client
             client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8090));
             // client.Connect(new IPEndPoint(IPAddress.Parse("101.200.138.237"), 8090));
             // client.Connect(new DnsEndPoint("ja.youlijinfu.com", 8090));
-            var buffer = new byte[1024];
-            var data = client.Receive(buffer);
-            System.Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+            var clientWapper = new SocketClient(client);
+            clientWapper.receiveEvent += data =>
+            {
+                System.Console.WriteLine($"Server {DateTime.Now}:{Encoding.UTF8.GetString(data)}");
+            };
+            System.Console.WriteLine("Begin Client");
+            while (true)
+            {
+                clientWapper.Send(Console.ReadLine());
+            }
         }
+    }
+
+    class SocketClient
+    {
+        private Socket clientSocket;
+        // public string IP { get; set; }
+        // public int Port { get; set; }
+        // public SocketClient(string ip, string port)
+        // {
+        //     IP = ip
+        // }
+
+        public SocketClient(Socket client)
+        {
+            clientSocket = client;
+            Task.Run(() =>
+            {
+                var data = new byte[1024];
+                try
+                {
+                    var rst = clientSocket.Receive(data);
+                    receiveEvent(data);
+                }
+                catch (SocketException)
+                {
+                    System.Console.WriteLine("Server 断开");
+                }
+            });
+        }
+
+        public void Send(byte[] data)
+        {
+            clientSocket.Send(data);
+        }
+
+        public void Send(string message)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
+        }
+
+        // 接收消息的事件
+        public event Action<byte[]> receiveEvent;
     }
 }
--- Chat.Server/Program.cs
+++ Chat.Server/Program.cs
@@ -17,13 +17,63 @@ namespace Chat.Server
             {
                 while (true)
                 {
-                    var client = server.Accept();
-                    System.Console.WriteLine("Client 请求：" + DateTime.Now);
-                    client.Send(Encoding.UTF8.GetBytes("Hello Server"));
+                    var clientWapper = new SocketClient(server.Accept());
+                    clientWapper.receiveEvent += data =>
+                    {
+                        System.Console.WriteLine("收到消息:" + Encoding.UTF8.GetString(data));
+                    };
+                    clientWapper.Send("Hello Server");
                 }
             });
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
+    }
+
+    // SocketWapper
+    class SocketClient
+    {
+        // Accept创建的socket
+        private Socket clientSocket;
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public SocketClient(Socket client)
+        {
+            System.Console.WriteLine($"{DateTime.Now} {(client.RemoteEndPoint as IPEndPoint).Address}");
+            var ipAddress = (client.RemoteEndPoint as IPEndPoint);
+            IP = ipAddress.Address.ToString();
+            Port = ipAddress.Port;
+            clientSocket = client;
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var data = new byte[1024];
+                    try
+                    {
+                        var rst = clientSocket.Receive(data);
+                        receiveEvent(data);
+                    }
+                    catch (SocketException)
+                    {
+                        System.Console.WriteLine("Client 离开");
+                    }
+                }
+            });
+        }
+
+        // 发消息给客户端
+        public void Send(byte[] data)
+        {
+            clientSocket.Send(data);
+        }
+
+        public void Send(string message)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
+        }
+
+        // 接收消息的事件
+        public event Action<byte[]> receiveEvent;
     }
 }
